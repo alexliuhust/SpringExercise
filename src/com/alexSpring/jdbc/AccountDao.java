b91bd694897b75b21a2500d89c4f0673e4433a62
Zehua(Alexander) Liu <68038532+alexliuhust@users.noreply.github.com>
2020-10-03 18:34:07 -0400

    JdbcTemplate update()

---
+++ src/com/alexSpring/jdbc/AccountDao.java
@@ -0,0 +1,8 @@
+package com.alexSpring.jdbc;
+
+public interface AccountDao {
+
+	public int addAccount(Account account);
+	public int updateAccount(Account account);
+	public int deleteAccount(int id);
+}
--- src/com/alexSpring/jdbc/JdbcTemplateTest.java
+++ src/com/alexSpring/jdbc/JdbcTemplateTest.java
@@ -9,6 +9,7 @@ public class JdbcTemplateTest {
 
 	/**
 	 * Use execute() to create tables
+	 * 
 	 * @param args
 	 */
 //	public static void main(String[] args) {
@@ -28,8 +29,7 @@ public class JdbcTemplateTest {
 //		
 //
 //	}
-	
-	
+
 	/**
 	 * Learn junit4 Test
 	 */
@@ -37,17 +37,71 @@ public class JdbcTemplateTest {
 	public void mainTest() {
 		// Load config file
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-		
+
 		// Obtain JDBC Template instance
 		JdbcTemplate jdbcTemplate = (JdbcTemplate) applicationContext.getBean("jdbcTemplate");
-		
+
 		// Use execute() to execute SQL and create Account Table
-		jdbcTemplate.execute("create table account("
-						   + "id int primary key auto_increment,"
-						   + "userName varchar(50),"
-						   + "balance double)");
-		
+		jdbcTemplate.execute("create table account(" 
+							+ "id int primary key auto_increment," 
+							+ "userName varchar(50),"
+							+ "balance double)");
+
 		System.out.println("Table Account succesfully created!");
+	}
+
+	@Test
+	public void addTest() {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		AccountDao accountDao = (AccountDao) applicationContext.getBean("accountDao");
+
+		Account account = new Account();
+		account.setUsername("Alexander");
+		account.setBalance(0.00);
+
+		int num = accountDao.addAccount(account);
+		if (num > 0) {
+			if (num == 1)
+				System.out.println("Successfully inserted 1 record!");
+			else
+				System.out.println("Successfully inserted " + num + " record(s)!");
+		} else
+			System.out.println("Failed to insert!");
+
+	}
+
+	@Test
+	public void updateTest() {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		AccountDao accountDao = (AccountDao) applicationContext.getBean("accountDao");
+
+		Account account = new Account();
+		account.setId(1);
+		account.setUsername("Atkinson Smith");
+		account.setBalance(1000.29);
+
+		int num = accountDao.updateAccount(account);
+		if (num > 0) {
+			if (num == 1)
+				System.out.println("Successfully updated 1 record!");
+			else
+				System.out.println("Successfully updated " + num + " record(s)!");
+		} else
+			System.out.println("Failed to update!");
+	}
+
+	@Test
+	public void deleteTest() {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		AccountDao accountDao = (AccountDao) applicationContext.getBean("accountDao");
+		int num = accountDao.deleteAccount(3);
+		if (num > 0) {
+			if (num == 1)
+				System.out.println("Successfully deleted 1 record!");
+			else
+				System.out.println("Successfully deleted " + num + " record(s)!");
+		} else
+			System.out.println("Failed to delete!");
 	}
 
 }
